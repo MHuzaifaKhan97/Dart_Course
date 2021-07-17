c57654f54bc0c1e73daffef972c39c4a6bd86d3f
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-07-17 18:15:25 +0500

    07: Loops in Dart

--- dart_course/bin/7_loops_in_dart/loops_in_dart.dart
+++ dart_course/bin/7_loops_in_dart/loops_in_dart.dart
@@ -1,3 +1,23 @@
 void main(List<String> args) {
   // Loops In Dart
+  // While Loop
+  var sum = 1;
+  while (sum < 10) {
+    sum += 1;
+    if (sum % 2 == 0) print(sum);
+  }
+
+  // Do While
+  int i = 0;
+  do {
+    i += 1;
+    print(i);
+  } while (i < 10);
+
+  // Break & Continue
+
+  // For Loop
+  for (int i = 10; i <= 100; i += 10) {
+    print(i);
+  }
 }
