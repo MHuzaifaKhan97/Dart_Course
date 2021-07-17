976f688c612c24cb36a83242ecb432c91a9ebf48
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2021-07-17 14:27:08 +0500

    01: Statements, Expressions, Opearations.

---
+++ dart_course/bin/1_statement_expression_operations/statements_exp_opr.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // Statements
+  print('Statement One!');
+  print('Statement Two!');
+
+  // Expressions
+  print(1 + 2);
+  // Opearations
+  // +, -, *, /, % etc
+  print(2 + 3);
+  print(2 - 3);
+  print(2 * 3);
+  print(2 / 3);
+  print(5 ~/ 3);
+  print(2 % 3);
+}
