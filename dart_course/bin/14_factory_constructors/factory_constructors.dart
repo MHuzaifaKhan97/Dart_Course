44b62c45cd911c46d7971a99c165c7ff15f0425e
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-08-01 00:15:21 +0500

    Factory Constructor

---
+++ dart_course/bin/14_factory_constructors/factory_constructors.dart
@@ -0,0 +1,14 @@
+class Car {
+  String make;
+  String bodyType;
+
+  Car({this.make = '', this.bodyType = 'Truck'});
+  factory Car.suv() {
+    return Car(bodyType: 'SUV');
+  }
+}
+
+void main(List<String> args) {
+  var carSuv = Car.suv();
+  print(carSuv.bodyType);
+}
