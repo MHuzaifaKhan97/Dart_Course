9b43372506d130b3c1f4806baaca068db449f5c3
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-08-08 02:30:20 +0500

    26: Async Programming.

---
+++ dart_course/bin/26_asnyc_programming/async_programming.dart
@@ -0,0 +1,20 @@
+// Asynchronous Programming
+
+/* Async Programming refers to occurance of events independent of the main
+program  flow and ways to deal with such events
+*/
+
+//* Thread
+
+/*
+A thread is a sequence of instructions within a program that can be executed
+independently of other code
+*/
+
+// Parallelism => run multiple thread at a time
+// Concurrency => run one thread at a time
+
+// Event Looping
+/*
+Dart uses an event loop to execute tasks that had previously been postponed.
+ */
